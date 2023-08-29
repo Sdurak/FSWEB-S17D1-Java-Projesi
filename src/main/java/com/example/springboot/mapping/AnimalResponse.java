259d20d6adc01d47acea4825e597d73ccba00ba9
Sdurak <sdurak91@hotmail.com>
2023-08-29 16:06:54 +0300

    "Proje J5G1"

---
+++ src/main/java/com/example/springboot/mapping/AnimalResponse.java
@@ -0,0 +1,27 @@
+package com.example.springboot.mapping;
+
+import com.example.springboot.entity.Animal;
+
+public class AnimalResponse {
+    private Animal animal;
+    private String message;
+    private int status;
+
+    public AnimalResponse(Animal animal, String message, int status) {
+        this.animal = animal;
+        this.message = message;
+        this.status = status;
+    }
+
+    public Animal getAnimal() {
+        return animal;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+}
